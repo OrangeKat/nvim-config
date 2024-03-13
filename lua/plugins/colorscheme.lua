d22783d7a2d8777ffdded12e37eea32865725835
thomas.polo <thomas.polo@epita.fr>
2024-03-13 12:05:16 +0100

    lazyvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  { "sainnhe/everforest" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      vim.cmd("let g:everforest_background = 'hard'"),
+      colorscheme = "everforest",
+    },
+  },
+}
